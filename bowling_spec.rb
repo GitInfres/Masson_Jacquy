8a26438b5cef0d4258f3e91a4a1d1c0bb5ae4037
INFRES <loris.jac@gmail.com>
2014-03-31 16:09:18 +0200

    first commit

---
+++ bowling_spec.rb
@@ -0,0 +1,9 @@
+require_relative 'bowling'
+
+describe Bowling, "#score" do
+  it "returns 0 for all gutter game" do
+    bowling = Bowling.new
+    20.times { bowling.hit(0) }
+    bowling.score.should eq(0)
+  end
+end
