94f4f23d340e2c04ddeae8358174c50aaab46c19
INFRES <loris.jac@gmail.com>
2014-04-02 14:15:30 +0200

    update

---
+++ tetris/tetris_spec2.rb
@@ -0,0 +1,10 @@
+require_relative 'Tetris'
+
+describe Tetris, "#tetris1" do
+  it "turn_right" do
+    tetris = Tetris.new
+    a1=[[0,0,1,0],[0,0,1,0],[0,1,1,0],[0,0,0,0]]
+    a2=[[0,0,0,0],[0,1,0,0],[0,1,1,1],[0,0,0,0]]
+    a1.should eq(a2)
+  end
+end
